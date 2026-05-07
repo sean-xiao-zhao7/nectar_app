6622ab7085a28315531c762e7fa6b9e2386c52b1
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-05-07 17:55:10 -0400

    Enable firebase auth sign up

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:nectar_app/components/layout/my_drawer.dart';
 import 'package:nectar_app/components/text/my_regular_text.dart';
 import 'package:nectar_app/helpers/form_helper.dart';
 import 'package:nectar_app/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 /// Register a new user
 class RegisterScreen extends StatefulWidget {
@@ -31,20 +32,50 @@ class _RegisterScreenState extends State<RegisterScreen> {
     super.dispose();
   }
 
-  void _submit() {
+  // sign up using password firebase call
+  Future<String> _signUp() async {
+    String message = '';
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        message = 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'The account already exists for that email.';
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(message)),
+        );
+      }
+    }
+
+    return message;
+  }
+
+  void _submit() async {
     if (!_formKey.currentState!.validate()) {
       return;
     }
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Sign up successful.')),
-    );
+    String result = await _signUp();
+    if (result.isEmpty) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Sign up successful.')),
+        );
 
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const HomeScreen(),
-      ),
-    );
+        Navigator.of(context).push(
+          MaterialPageRoute<void>(
+            builder: (_) => const HomeScreen(),
+          ),
+        );
+      }
+    }
   }
 
   @override
