ae45fdd7d1b3e2ec635997bfc22c7b98ed88480b
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-06-11 15:17:20 -0400

    Add displayName split logic to add card screen

--- lib/screens/cards/add_single_card_screen.dart
+++ lib/screens/cards/add_single_card_screen.dart
@@ -73,6 +73,12 @@ class _AddSingleCardScreenState extends State<AddSingleCardScreen> {
             );
           } else if (snapshotAuth.connectionState == ConnectionState.active &&
               snapshotAuth.hasData) {
+            // Split displayName into first/last names.
+            String fullName = snapshotAuth.data!.displayName!;
+            int lastSpaceIndex = fullName.lastIndexOf(' ');
+            _firstNameController.text = fullName.substring(0, lastSpaceIndex);
+            _lastNameController.text = fullName.substring(lastSpaceIndex + 1);
+
             widgetTree = FutureBuilder<List<NectarCard>>(
                 future: fetchUserAllCards(snapshotAuth.data!.uid),
                 builder: (BuildContext context,
@@ -81,7 +87,7 @@ class _AddSingleCardScreenState extends State<AddSingleCardScreen> {
                       snapshotCards.hasData) {
                     return ListView(shrinkWrap: true, children: [
                       MyRegularText(
-                        'Please fill in some details for your new card. Other than first/last names, other fields can be filled out later.',
+                        'Please fill in some details for your new card. Other than first/last names, other fields can be filled out later if you need.',
                         textAlign: TextAlign.center,
                       ),
                       SizedBox(
@@ -239,6 +245,7 @@ class _AddSingleCardScreenState extends State<AddSingleCardScreen> {
                       padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
                       child: MyRegularButton(
                         label: 'Complete adding a new card',
+                        iconData: Icons.done,
                         onPressed: _isFormValid
                             ? () => addNewCardFormHelper(
                                 context,
