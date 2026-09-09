6d9d5fd336a71cd76f4168a5e01d82edb21a18c5
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-09-09 14:59:52 -0400

    Update card screen
    Fix styling.
    Properly display all info if not empty.

--- lib/screens/cards/single_card_screen.dart
+++ lib/screens/cards/single_card_screen.dart
@@ -39,20 +39,93 @@ class _SingleCardScreenState extends State<SingleCardScreen> {
               spacing: 20,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                MyLargeText(widget.nectarCard.mainName),
+                MyLargeText(
+                  widget.nectarCard.mainName,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                if (widget.nectarCard.shortDescription != '')
+                  MyRegularText(widget.nectarCard.shortDescription),
+                if (widget.nectarCard.personalInfo['firstName'] != '' &&
+                    widget.nectarCard.personalInfo['lastName'] != '')
+                  Divider(
+                    height: 5,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                if (widget.nectarCard.personalInfo['firstName'] != '' &&
+                    widget.nectarCard.personalInfo['lastName'] != '')
+                  MyLargeText(
+                    'Personal',
+                  ),
+                if (widget.nectarCard.personalInfo['firstName'] != '' &&
+                    widget.nectarCard.personalInfo['lastName'] != '')
+                  MyRegularText(
+                      'Full name: ${widget.nectarCard.personalInfo['firstName']} ${widget.nectarCard.personalInfo['lastName']}'),
+                if (widget.nectarCard.personalInfo['email'] != '')
+                  MyRegularText(
+                      'Email: ${widget.nectarCard.personalInfo['email']}'),
+                if (widget.nectarCard.personalInfo['phone'] != '')
+                  MyRegularText(
+                      'Phone: ${widget.nectarCard.personalInfo['phone']}'),
                 Divider(
                   height: 5,
                   color: Theme.of(context).colorScheme.primary,
                 ),
+                MyLargeText(
+                  'Company',
+                ),
                 MyRegularText(
-                    'First name: ${widget.nectarCard.personalInfo['firstName']}'),
+                    '${widget.nectarCard.companyInfo['companyName']}'),
                 MyRegularText(
-                    'Last name: ${widget.nectarCard.personalInfo['lastName']}'),
-                MyRegularText(
-                    'Email: ${widget.nectarCard.personalInfo['email']}'),
-                MyRegularText('Role: ${widget.nectarCard.companyInfo['job']}'),
-                MyRegularText(
-                    'Company: ${widget.nectarCard.companyInfo['company']}'),
+                    '${widget.nectarCard.companyInfo['businessType']}'),
+                if (widget.nectarCard.companyInfo['role'] != '')
+                  MyRegularText(
+                      'Role: ${widget.nectarCard.companyInfo['role']}'),
+                if (widget.nectarCard.companyInfo['department'] != '')
+                  MyRegularText(
+                      'Department: ${widget.nectarCard.companyInfo['department']}'),
+                Divider(
+                  height: 5,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                MyLargeText(
+                  'Social Media',
+                ),
+                if (widget.nectarCard.socialMedia['website'] != '')
+                  MyRegularText('${widget.nectarCard.socialMedia['website']}'),
+                if (widget.nectarCard.socialMedia['linkedin'] != '')
+                  MyRegularText(
+                      'linkedin.com/${widget.nectarCard.socialMedia['linkedin']}'),
+                if (widget.nectarCard.socialMedia['twitter'] != '')
+                  MyRegularText(
+                      'x.com/${widget.nectarCard.socialMedia['twitter']}'),
+                if (widget.nectarCard.socialMedia['instagram'] != '')
+                  MyRegularText(
+                      'instagram.com/${widget.nectarCard.socialMedia['instagram']}'),
+                if (widget.nectarCard.socialMedia['facebook'] != '')
+                  MyRegularText(
+                      'facebook.com/${widget.nectarCard.socialMedia['facebook']}'),
+                Divider(
+                  height: 5,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                MyLargeText(
+                  'Location',
+                ),
+                if (widget.nectarCard.addressInfo['street'] != '')
+                  MyRegularText(
+                      'Street: ${widget.nectarCard.addressInfo['street']}'),
+                if (widget.nectarCard.addressInfo['city'] != '')
+                  MyRegularText(
+                      'City: ${widget.nectarCard.addressInfo['city']}'),
+                if (widget.nectarCard.addressInfo['state'] != '')
+                  MyRegularText(
+                      'State/Province: ${widget.nectarCard.addressInfo['state']}'),
+                if (widget.nectarCard.addressInfo['country'] != '')
+                  MyRegularText(
+                      'Country: ${widget.nectarCard.addressInfo['country']}'),
+                if (widget.nectarCard.addressInfo['postalCode'] != '')
+                  MyRegularText(
+                      'Postal: ${widget.nectarCard.addressInfo['postalCode']}'),
               ],
             ),
           ),
