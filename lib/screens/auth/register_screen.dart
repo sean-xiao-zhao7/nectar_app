e423910046fa0274cf4299280723e7370b690557
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-09-19 19:17:19 -0400

    Fix register/login screens
    Use NectarScaffoldContainer to fix height issue.

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:nectar_app/components/buttons/nectar_regular_button.dart';
 import 'package:nectar_app/components/layout/nectar_app_bar.dart';
-import 'package:nectar_app/components/layout/nectar_drawer.dart';
+import 'package:nectar_app/components/layout/nectar_scaffold_container.dart';
 import 'package:nectar_app/components/text/nectar_regular_text.dart';
 
 import 'package:nectar_app/helpers/auth_helper.dart';
@@ -35,112 +35,115 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: NectarDrawer(),
-      appBar: NectarAppBar(
+    return NectarScaffoldContainer(
+      title: 'Sign up',
+      appBarLead: NectarAppBar(
         title: 'Sign up',
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(32),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: <Widget>[
-                NectarRegularButton(
-                  label: 'Log in with Google',
-                  onPressed: () => authFormSubmitGoogleHelper(context),
-                  iconData: Icons.g_mobiledata_sharp,
-                ),
-                const SizedBox(height: 24),
-                const NectarRegularText('OR'),
-                const SizedBox(height: 24),
-                const NectarRegularText(
-                    'A few details to get started with Nectar.'),
-                const SizedBox(height: 24),
-                myTextFormField(
-                  context: context,
-                  controller: _firstNameController,
-                  labelText: 'First name',
-                  capitalize: true,
-                  textInputAction: TextInputAction.next,
-                  validators: <FormFieldValidatorFn>[
-                    FormValidators.required('First name'),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                myTextFormField(
-                  context: context,
-                  controller: _lastNameController,
-                  labelText: 'Last name',
-                  capitalize: true,
-                  textInputAction: TextInputAction.next,
-                  validators: <FormFieldValidatorFn>[
-                    FormValidators.required('Last name'),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                myTextFormField(
-                  context: context,
-                  controller: _emailController,
-                  labelText: 'Email',
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
-                  validators: <FormFieldValidatorFn>[
-                    FormValidators.email(),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                myTextFormField(
-                  context: context,
-                  controller: _passwordController,
-                  labelText: 'Password',
-                  obscureText: true,
-                  textInputAction: TextInputAction.done,
-                  maxLines: 1,
-                  validators: <FormFieldValidatorFn>[
-                    FormValidators.minLength(6, 'Password'),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Form(
+                key: formKey,
+                child: Column(
                   children: <Widget>[
-                    Checkbox(
-                      value: _isAgreementChecked,
-                      onChanged: (value) {
-                        setState(() {
-                          _isAgreementChecked = value ?? false;
-                        });
-                      },
+                    NectarRegularButton(
+                      label: 'Log in with Google',
+                      onPressed: () => authFormSubmitGoogleHelper(context),
+                      iconData: Icons.g_mobiledata_sharp,
                     ),
-                    const Expanded(
-                      child: NectarRegularText(
-                        'I agree to Nectar\'s Terms of Service and Privacy Policy.',
-                      ),
+                    const SizedBox(height: 24),
+                    const NectarRegularText('OR'),
+                    const SizedBox(height: 24),
+                    const NectarRegularText(
+                        'A few details to get started with Nectar.'),
+                    const SizedBox(height: 24),
+                    myTextFormField(
+                      context: context,
+                      controller: _firstNameController,
+                      labelText: 'First name',
+                      capitalize: true,
+                      textInputAction: TextInputAction.next,
+                      validators: <FormFieldValidatorFn>[
+                        FormValidators.required('First name'),
+                      ],
                     ),
+                    const SizedBox(height: 24),
+                    myTextFormField(
+                      context: context,
+                      controller: _lastNameController,
+                      labelText: 'Last name',
+                      capitalize: true,
+                      textInputAction: TextInputAction.next,
+                      validators: <FormFieldValidatorFn>[
+                        FormValidators.required('Last name'),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    myTextFormField(
+                      context: context,
+                      controller: _emailController,
+                      labelText: 'Email',
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      validators: <FormFieldValidatorFn>[
+                        FormValidators.email(),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    myTextFormField(
+                      context: context,
+                      controller: _passwordController,
+                      labelText: 'Password',
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                      maxLines: 1,
+                      validators: <FormFieldValidatorFn>[
+                        FormValidators.minLength(6, 'Password'),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Checkbox(
+                          value: _isAgreementChecked,
+                          onChanged: (value) {
+                            setState(() {
+                              _isAgreementChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        const Expanded(
+                          child: NectarRegularText(
+                            'I agree to Nectar\'s Terms of Service and Privacy Policy.',
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    NectarRegularButton(
+                      label: 'Sign up to Nectar',
+                      onPressed: _isAgreementChecked
+                          ? () => authFormSubmitHelper(
+                              context,
+                              formKey,
+                              signUpHelper,
+                              {
+                                'firstName': _firstNameController.text,
+                                'lastName': _lastNameController.text,
+                                'email': _emailController.text,
+                                'password': _passwordController.text,
+                              },
+                              'Sign up successful')
+                          : null,
+                    )
                   ],
                 ),
-                const SizedBox(height: 24),
-                NectarRegularButton(
-                  label: 'Sign up to Nectar',
-                  onPressed: _isAgreementChecked
-                      ? () => authFormSubmitHelper(
-                          context,
-                          formKey,
-                          signUpHelper,
-                          {
-                            'firstName': _firstNameController.text,
-                            'lastName': _lastNameController.text,
-                            'email': _emailController.text,
-                            'password': _passwordController.text,
-                          },
-                          'Sign up successful')
-                      : null,
-                )
-              ],
-            ),
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
